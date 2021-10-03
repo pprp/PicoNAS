7cfc7d3d68d7d27871aa556d711020213ec64c01
pprp <1115957667@qq.com>
2021-10-03 09:50:22 +0800

    update random train spos

---
+++ Search.py
@@ -0,0 +1,174 @@
+import argparse
+import os
+import time
+
+import torch
+import torch.nn as nn
+import torchvision
+from thop import profile
+from torchsummary import summary
+from torchvision import datasets
+from tqdm import tqdm
+
+import utils
+from network import SinglePathOneShotCIFAR
+from utils import data_transforms
+
+
+def get_args():
+    parser = argparse.ArgumentParser("Single_Path_One_Shot")
+    parser.add_argument('--exp_name', type=str, default='spos_cifar10',
+                        required=True, help='experiment name')
+    parser.add_argument('--data_dir', type=str,
+                        default='/home/pdluser/datasets/', help='path to the dataset')
+    parser.add_argument('--classes', type=int, default=10,
+                        help='dataset classes')
+    parser.add_argument('--layers', type=int, default=20, help='batch size')
+    parser.add_argument('--num_choices', type=int, default=4,
+                        help='number choices per layer')
+    parser.add_argument('--batch_size', type=int,
+                        default=96, help='batch size')
+    parser.add_argument('--epochs', type=int, default=600, help='batch size')
+    parser.add_argument('--learning_rate', type=float,
+                        default=0.025, help='initial learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+    parser.add_argument('--weight-decay', type=float,
+                        default=3e-4, help='weight decay')
+    parser.add_argument('--val_interval', type=int, default=5,
+                        help='validate and save frequency')
+    parser.add_argument('--random_search', type=int,
+                        default=1000, help='validate and save frequency')
+    # ******************************* dataset *******************************#
+    parser.add_argument('--dataset', type=str,
+                        default='cifar10', help='path to the dataset')
+    parser.add_argument('--cutout', action='store_true', help='use cutout')
+    parser.add_argument('--cutout_length', type=int,
+                        default=16, help='cutout length')
+    parser.add_argument('--auto_aug', action='store_true',
+                        default=False, help='use auto augmentation')
+    parser.add_argument('--resize', action='store_true',
+                        default=False, help='use resize')
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
+def train(args, epoch, train_data, device, model, criterion, optimizer, scheduler, supernet):
+    model.train()
+    train_loss = 0.0
+    top1 = utils.AvgrageMeter()
+    train_data = tqdm(train_data)
+    train_data.set_description(
+        '[%s%04d/%04d %s%f]' % ('Epoch:', epoch + 1, args.epochs, 'lr:', scheduler.get_lr()[0]))
+    for step, (inputs, targets) in enumerate(train_data):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        if supernet:
+            choice = utils.random_choice(args.num_choices, args.layers)
+            outputs = model(inputs, choice)
+        else:
+            outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        # if args.dataset == 'cifar10':
+        loss.backward()
+        # elif args.dataset == 'imagenet':
+        #     with amp.scale_loss(loss, optimizer) as scaled_loss:
+        #         scaled_loss.backward()
+        optimizer.step()
+        prec1, prec5 = utils.accuracy(outputs, targets, topk=(1, 5))
+        n = inputs.size(0)
+        top1.update(prec1.item(), n)
+        train_loss += loss.item()
+        postfix = {'train_loss': '%.6f' % (
+            train_loss / (step + 1)), 'train_acc': '%.6f' % top1.avg}
+        train_data.set_postfix(log=postfix)
+
+
+def validate(args, epoch, val_data, device, model, criterion, supernet, choice=None):
+    model.eval()
+    val_loss = 0.0
+    val_top1 = utils.AvgrageMeter()
+    with torch.no_grad():
+        for step, (inputs, targets) in enumerate(val_data):
+            inputs, targets = inputs.to(device), targets.to(device)
+            if supernet:
+                if choice == None:
+                    choice = utils.random_choice(args.num_choices, args.layers)
+                outputs = model(inputs, choice)
+            else:
+                outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            val_loss += loss.item()
+            prec1, prec5 = utils.accuracy(outputs, targets, topk=(1, 5))
+            n = inputs.size(0)
+            val_top1.update(prec1.item(), n)
+        print('[Val_Accuracy epoch:%d] val_loss:%f, val_acc:%f'
+              % (epoch + 1, val_loss / (step + 1), val_top1.avg))
+        return val_top1.avg
+
+
+def main():
+    # args & device
+    args = get_args()
+    if torch.cuda.is_available():
+        print('Train on GPU!')
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    # dataset
+    assert args.dataset in ['cifar10', 'imagenet']
+    train_transform, valid_transform = data_transforms(args)
+    if args.dataset == 'cifar10':
+        trainset = torchvision.datasets.CIFAR10(root=os.path.join(args.data_dir, 'cifar'), train=True,
+                                                download=True, transform=train_transform)
+        train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                                   shuffle=True, pin_memory=True, num_workers=8)
+        valset = torchvision.datasets.CIFAR10(root=os.path.join(args.data_dir, 'cifar'), train=False,
+                                              download=True, transform=valid_transform)
+        val_loader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size,
+                                                 shuffle=False, pin_memory=True, num_workers=8)
+    elif args.dataset == 'imagenet':
+        train_data_set = datasets.ImageNet(os.path.join(
+            args.data_dir, 'ILSVRC2012', 'train'), train_transform)
+        val_data_set = datasets.ImageNet(os.path.join(
+            args.data_dir, 'ILSVRC2012', 'valid'), valid_transform)
+        train_loader = torch.utils.data.DataLoader(train_data_set, batch_size=args.batch_size, shuffle=True,
+                                                   num_workers=8, pin_memory=True, sampler=None)
+        val_loader = torch.utils.data.DataLoader(val_data_set, batch_size=args.batch_size, shuffle=False,
+                                                 num_workers=8, pin_memory=True)
+
+    # SinglePath_OneShot
+    model = SinglePathOneShotCIFAR(
+        args.dataset, args.resize, args.classes, args.layers)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(
+        model.parameters(), args.learning_rate, args.momentum, args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer, lambda epoch: 1 - (epoch / args.epochs))
+
+    # flops & params & structure
+    flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32),) if args.dataset == 'cifar10'
+                            else (torch.randn(1, 3, 224, 224),), verbose=False)
+    # print(model)
+    print('Random Path of the Supernet: Params: %.2fM, Flops:%.2fM' %
+          ((params / 1e6), (flops / 1e6)))
+    model = model.to(device)
+    summary(model, (3, 32, 32) if args.dataset == 'cifar10' else (3, 224, 224))
+
+    # train supernet
+    start = time.time()
+    for epoch in range(args.epochs):
+        train(args, epoch, train_loader, device, model,
+              criterion, optimizer, scheduler, supernet=True)
+        scheduler.step()
+        if (epoch + 1) % args.val_interval == 0:
+            validate(args, epoch, val_loader, device,
+                     model, criterion, supernet=True)
+            utils.save_checkpoint(
+                {'state_dict': model.state_dict(), }, epoch + 1, tag=args.exp_name + '_super')
+    utils.time_record(start)
+
+
+if __name__ == '__main__':
+    main()
--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import enum
 import torch
 import numpy as np
 import torch.nn as nn
@@ -132,9 +133,9 @@ class ShuffleXModule(nn.Module):
         return y
 
 
-class SinglePathOneShotCIFAR(nn.Module):
+class SinglePathOneShotSuperNet(nn.Module):
     def __init__(self, dataset='cifar10', input_size=32, classes=10, layers=20):
-        super(SinglePathOneShotCIFAR, self).__init__()
+        super(SinglePathOneShotSuperNet, self).__init__()
         if dataset == 'cifar10':
             first_stride = 1
             self.downsample_layers = [4, 8]
@@ -215,20 +216,112 @@ class SinglePathOneShotCIFAR(nn.Module):
     def forward(self, input, choice=np.random.randint(4, size=20)):
         # 20 = 4+4+8+4
         x = self.first_conv(input)
-        print(x.shape)
         for i, j in enumerate(choice):
             x = self.features[i][j](x)
-            print(x.shape)
         x = self.last_conv(x)
-        print(x.shape)
         x = self.gvp(x)
         x = x.view(-1, self.last_channel)
-        print(x.shape)
         x = self.classifier(x)
         return x
 
+class SinglePathOneShotSubNet(nn.Module):
+    def __init__(self, dataset='cifar10', input_size=32, classes=10, layers=20, choice=None):
+        super(SinglePathOneShotSubNet, self).__init__()
+        if dataset == 'cifar10':
+            first_stride = 1
+            self.downsample_layers = [4, 8]
+        
+        self.choice = np.random.randint(4, size=20) if choice == None else choice
 
+        # ShuffleNet config
+        self.last_channel = 1024
+        self.channel = [16,
+                        64, 64, 64, 64,
+                        160, 160, 160, 160,
+                        320, 320, 320, 320, 320, 320, 320, 320,
+                        640, 640, 640, 640]
+
+        self.kernel_list = [3, 5, 7, 'x']
+
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(3, self.channel[0], kernel_size=3,
+                      stride=first_stride, padding=1, bias=False),  # bias ?
+            nn.BatchNorm2d(self.channel[0], affine=False),  # affine ?
+            nn.ReLU6(inplace=True)
+        )
+
+        self.choice_block = nn.ModuleList([])
+        self.features = nn.ModuleList([])
+
+        for i,c in enumerate(self.choice):
+            if i in self.downsample_layers:
+                stride = 2
+                inc, ouc = self.channel[i], self.channel[i+1]
+            else:
+                stride = 1
+                inc, ouc = self.channel[i] // 2, self.channel[i+1]
+            layer = nn.ModuleList([])
+
+            j = self.kernel_list[c]
+
+            if j == 'x':
+                layer.append(ShuffleXModule(inc, ouc, stride=stride))
+            else:
+                layer.append(ShuffleModule(
+                    inc, ouc, kernel=j, stride=stride))
+                    
+            self.features.append(layer)
+
+        self.last_conv = nn.Sequential(
+            nn.Conv2d(self.channel[-1],
+                      self.last_channel, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.last_channel, affine=False),
+            nn.ReLU6(inplace=True)
+        )
+
+        self.gvp = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(0.1)
+        self.classifier = nn.Linear(self.last_channel, classes, bias=False)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for name, m in self.named_modules():
+            if isinstance(m, nn.Conv2d):
+                if 'first' in name:
+                    nn.init.normal_(m.weight, 0, 0.01)
+                else:
+                    nn.init.normal_(m.weight, 0, 1.0 / m.weight.shape[1])
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                if m.weight is not None:
+                    nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, input, ):
+        # 20 = 4+4+8+4
+        x = self.first_conv(input)
+        for i, j in enumerate(choice):
+            x = self.features[i][j](x)
+        x = self.last_conv(x)
+        x = self.gvp(x)
+        x = x.view(-1, self.last_channel)
+        x = self.classifier(x)
+        return x
+    
 if __name__ == "__main__":
-    m = SinglePathOneShotCIFAR()
+    m = SinglePathOneShotSuperNet()
     input = torch.zeros(5, 3, 32, 32)
     print(m(input).shape)
