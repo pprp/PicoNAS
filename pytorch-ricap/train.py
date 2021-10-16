e8f4f6664bf57373b03e035f8e1111f55882ac45
PJDong <1115957667@qq.com>
2021-10-16 21:58:45 +0800

    update apex

---
+++ pytorch-ricap/train.py
@@ -0,0 +1,377 @@
+from enum import auto
+import os
+import argparse
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+import joblib
+from collections import OrderedDict
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from utils import *
+from wide_resnet import *
+
+# from torchstat import stat 
+from torch.cuda.amp import autocast as autocast
+from torch.cuda.amp import GradScaler as GradScaler
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--name', default=None,
+                        help='model name: (default: cifar10_ricap)')
+    parser.add_argument('--dataset', default='cifar10',
+                        choices=['cifar10', 'cifar100'],
+                        help='dataset name')
+    parser.add_argument('--bs', default=128, type=int,
+                            help='use RICAP')
+    parser.add_argument('--nw', default=4, type=int,
+                            help='use RICAP')
+    parser.add_argument('--depth', default=28, type=int)
+    parser.add_argument('--width', default=10, type=int)
+    parser.add_argument('--ricap', default=True, type=str2bool,
+                        help='use RICAP')
+    parser.add_argument('--ricap-beta', default=0.3, type=float)
+    parser.add_argument('--random-erase', default=False, type=str2bool,
+                        help='use Random Erasing')
+    parser.add_argument('--random-erase-prob', default=0.5, type=float)
+    parser.add_argument('--random-erase-sl', default=0.02, type=float)
+    parser.add_argument('--random-erase-sh', default=0.4, type=float)
+    parser.add_argument('--random-erase-r', default=0.3, type=float)
+    parser.add_argument('--mixup', default=False, type=str2bool,
+                        help='use Mixup')
+    parser.add_argument('--mixup-alpha', default=1.0, type=float)
+    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--lr', '--learning-rate', default=1e-1, type=float)
+    parser.add_argument('--milestones', default='60,120,160', type=str)
+    parser.add_argument('--gamma', default=0.2, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
+    parser.add_argument('--weight-decay', default=5e-4, type=float)
+    parser.add_argument('--nesterov', default=False, type=str2bool)
+    parser.add_argument('--amp', type=str2bool, default=False)
+
+    args = parser.parse_args()
+
+
+    if args.amp:
+        args.scaler = GradScaler()
+
+    return args
+
+
+def train(args, train_loader, model, criterion, optimizer, epoch, scheduler=None):
+    losses = AverageMeter()
+    scores = AverageMeter()
+
+    model.train()
+
+    for i, (input, target) in tqdm(enumerate(train_loader), total=len(train_loader)):
+        # from original paper's appendix
+        if args.ricap:
+            I_x, I_y = input.size()[2:]
+
+            w = int(np.round(I_x * np.random.beta(args.ricap_beta, args.ricap_beta)))
+            h = int(np.round(I_y * np.random.beta(args.ricap_beta, args.ricap_beta)))
+            w_ = [w, I_x - w, w, I_x - w]
+            h_ = [h, h, I_y - h, I_y - h]
+
+            cropped_images = {}
+            c_ = {}
+            W_ = {}
+            for k in range(4):
+                idx = torch.randperm(input.size(0))
+                x_k = np.random.randint(0, I_x - w_[k] + 1)
+                y_k = np.random.randint(0, I_y - h_[k] + 1)
+                cropped_images[k] = input[idx][:, :, x_k:x_k + w_[k], y_k:y_k + h_[k]]
+                c_[k] = target[idx].cuda()
+                W_[k] = w_[k] * h_[k] / (I_x * I_y)
+
+            patched_images = torch.cat(
+                (torch.cat((cropped_images[0], cropped_images[1]), 2),
+                torch.cat((cropped_images[2], cropped_images[3]), 2)),
+            3)
+            patched_images = patched_images.cuda()
+
+            if args.amp:
+                with autocast():
+                    output = model(patched_images)
+                    loss = sum([W_[k] * criterion(output, c_[k]) for k in range(4)])
+            else:
+                output = model(patched_images)
+                loss = sum([W_[k] * criterion(output, c_[k]) for k in range(4)])
+
+            acc = sum([W_[k] * accuracy(output, c_[k])[0] for k in range(4)])
+
+        elif args.mixup:
+            l = np.random.beta(args.mixup_alpha, args.mixup_alpha)
+
+            idx = torch.randperm(input.size(0))
+            input_a, input_b = input, input[idx]
+            target_a, target_b = target, target[idx]
+
+            mixed_input = l * input_a + (1 - l) * input_b
+
+            target_a = target_a.cuda()
+            target_b = target_b.cuda()
+            mixed_input = mixed_input.cuda()
+
+            output = model(mixed_input)
+            loss = l * criterion(output, target_a) + (1 - l) * criterion(output, target_b)
+
+            acc = l * accuracy(output, target_a)[0] + (1 - l) * accuracy(output, target_b)[0]
+
+        else:
+            input = input.cuda()
+            target = target.cuda()
+
+            output = model(input)
+            loss = criterion(output, target)
+
+            acc = accuracy(output, target)[0]
+
+
+
+        losses.update(loss.item(), input.size(0))
+        scores.update(acc.item(), input.size(0))
+
+        # compute gradient and do optimizing step
+        if args.amp:
+            args.scaler.scale(loss).backward()
+            args.scaler.step(optimizer)
+            args.scaler.update()
+        else:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    log = OrderedDict([
+        ('loss', losses.avg),
+        ('acc', scores.avg),
+    ])
+
+    return log
+
+
+def validate(args, val_loader, model, criterion):
+    losses = AverageMeter()
+    scores = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        for i, (input, target) in tqdm(enumerate(val_loader), total=len(val_loader)):
+            input = input.cuda()
+            target = target.cuda()
+
+            output = model(input)
+            loss = criterion(output, target)
+
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+            losses.update(loss.item(), input.size(0))
+            scores.update(acc1.item(), input.size(0))
+
+    log = OrderedDict([
+        ('loss', losses.avg),
+        ('acc', scores.avg),
+    ])
+
+    return log
+
+
+def main():
+    args = parse_args()
+
+    if args.name is None:
+        args.name = '%s_WideResNet%s-%s' %(args.dataset, args.depth, args.width)
+        if args.ricap:
+            args.name += '_wRICAP'
+        if args.random_erase:
+            args.name += '_wRandomErasing'
+        if args.mixup:
+            args.name += '_wMixup'
+
+    if not os.path.exists('models/%s' %args.name):
+        os.makedirs('models/%s' %args.name)
+
+    print('Config -----')
+    for arg in vars(args):
+        print('%s: %s' %(arg, getattr(args, arg)))
+    print('------------')
+
+    with open('models/%s/args.txt' %args.name, 'w') as f:
+        for arg in vars(args):
+            print('%s: %s' %(arg, getattr(args, arg)), file=f)
+
+    joblib.dump(args, 'models/%s/args.pkl' %args.name)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    cudnn.benchmark = True
+
+    # data loading code
+    if args.dataset == 'cifar10':
+        if args.random_erase:
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                RandomErase(args.random_erase_prob, args.random_erase_sl,
+                            args.random_erase_sh, args.random_erase_r),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+            ])
+        else:
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+            ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        train_set = datasets.CIFAR10(
+            root='~/data',
+            train=True,
+            download=True,
+            transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(
+            train_set,
+            batch_size=args.bs,
+            shuffle=True,
+            num_workers=args.nw)
+
+        test_set = datasets.CIFAR10(
+            root='~/data',
+            train=False,
+            download=True,
+            transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(
+            test_set,
+            batch_size=128,
+            shuffle=False,
+            num_workers=8)
+
+        num_classes = 10
+
+    elif args.dataset == 'cifar100':
+        if args.random_erase:
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                RandomErase(args.random_erase_prob, args.random_erase_sl,
+                            args.random_erase_sh, args.random_erase_r),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+            ])
+        else:
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                     (0.2023, 0.1994, 0.2010)),
+            ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        train_set = datasets.CIFAR100(
+            root='~/data',
+            train=True,
+            download=True,
+            transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(
+            train_set,
+            batch_size=128,
+            shuffle=True,
+            num_workers=8)
+
+        test_set = datasets.CIFAR100(
+            root='~/data',
+            train=False,
+            download=True,
+            transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(
+            test_set,
+            batch_size=128,
+            shuffle=False,
+            num_workers=8)
+
+        num_classes = 100
+
+    # create model
+    model = WideResNet(args.depth, args.width, num_classes=num_classes)
+
+    print(count_params(model))
+
+    # stat(model, (3, 32, 32))
+
+    # from torchsummary import summary 
+    # summary(model, input_size=(3, 32, 32), batch_size=-1)
+
+    model = model.cuda()
+
+
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr,
+            momentum=args.momentum, weight_decay=args.weight_decay)
+
+    scheduler = lr_scheduler.MultiStepLR(optimizer,
+            milestones=[int(e) for e in args.milestones.split(',')], gamma=args.gamma)
+
+    log = pd.DataFrame(index=[], columns=[
+        'epoch', 'lr', 'loss', 'acc', 'val_loss', 'val_acc'
+    ])
+
+    best_acc = 0
+    for epoch in range(args.epochs):
+        print('Epoch [%d/%d]' %(epoch+1, args.epochs))
+
+        scheduler.step()
+
+        # train for one epoch
+        train_log = train(args, train_loader, model, criterion, optimizer, epoch)
+        # evaluate on validation set
+        val_log = validate(args, test_loader, model, criterion)
+
+        print('loss %.4f - acc %.4f - val_loss %.4f - val_acc %.4f'
+            %(train_log['loss'], train_log['acc'], val_log['loss'], val_log['acc']))
+
+        tmp = pd.Series([
+            epoch,
+            scheduler.get_lr()[0],
+            train_log['loss'],
+            train_log['acc'],
+            val_log['loss'],
+            val_log['acc'],
+        ], index=['epoch', 'lr', 'loss', 'acc', 'val_loss', 'val_acc'])
+
+        log = log.append(tmp, ignore_index=True)
+        log.to_csv('models/%s/log.csv' %args.name, index=False)
+
+        if val_log['acc'] > best_acc:
+            torch.save(model.state_dict(), 'models/%s/model.pth' %args.name)
+            best_acc = val_log['acc']
+            print("=> saved best model")
+
+
+if __name__ == '__main__':
+    main()
