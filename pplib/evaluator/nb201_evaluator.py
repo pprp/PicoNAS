:bug: fix bug in rank difference calculation

pplib/evaluator/nb201_evaluator.py
@@ -148,17 +148,19 @@ class NB201Evaluator(Evaluator):
         sorted_idx_by_flops = np.array(flops_indicator_list).argsort()
 
         # compute splited index by flops
+        range_length = len(sorted_idx_by_flops) // 5
         splited_idx_by_flops = [
-            sorted_idx_by_flops[i * 5:(i + 1) * 5]
-            for i in range(int(len(sorted_idx_by_flops) / 5) + 1)
-            if (sorted_idx_by_flops[i * 5:(i + 1) * 5]).any()
+            sorted_idx_by_flops[i * range_length:(i + 1) * range_length]
+            for i in range(int(len(sorted_idx_by_flops) / range_length) + 1)
+            if (sorted_idx_by_flops[i * range_length:(i + 1) *
+                                    range_length]).any()
         ]
 
         true_indicator_list = np.array(true_indicator_list)
         generated_indicator_list = np.array(generated_indicator_list)
 
         rd_list = []
-        for i in range(len(sorted_idx_by_flops) // 5):
+        for i in range(len(splited_idx_by_flops)):
             current_idx = np.array(splited_idx_by_flops[i])
             tmp_rd = rank_difference(true_indicator_list[current_idx],
                                      generated_indicator_list[current_idx])

pplib/utils/rank_consistency.py
@@ -67,10 +67,8 @@ def rank_difference(true_vector, pred_vector):
     return sum_rd / length
 
 
-# Calculate the BR@K, WR@K
-def minmax_n_at_k(true_scores,
-                  predict_scores,
-                  ks=[0.001, 0.005, 0.01, 0.05, 0.10, 0.20]):
+# Calculate the BR@K, WR@K [0-1]
+def minmax_n_at_k(true_scores, predict_scores, ks=[0.01, 0.05, 0.10, 0.50]):
     true_scores = np.array(true_scores)
     predict_scores = np.array(predict_scores)
     num_archs = len(true_scores)
@@ -90,9 +88,8 @@ def minmax_n_at_k(true_scores,
 
 
 # Calculate the P@topK, P@bottomK, and Kendall-Tau in predicted topK/bottomK
-def p_at_tb_k(true_scores,
-              predict_scores,
-              ratios=[0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]):
+# [0-1]
+def p_at_tb_k(true_scores, predict_scores, ratios=[0.01, 0.05, 0.1, 0.5]):
     predict_scores = np.array(predict_scores)
     true_scores = np.array(true_scores)
     predict_inds = np.argsort(predict_scores)[::-1]