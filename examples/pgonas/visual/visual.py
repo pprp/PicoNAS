cb59531b4f8b325a601e5f9054b6c86eb15a6cc4
PJDong <1115957667@qq.com>
2022-10-21 10:12:45 +0800

    :new: update style of plt

--- examples/pgonas/visual/visual.py
+++ examples/pgonas/visual/visual.py
@@ -2,7 +2,6 @@ import csv
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 plt.style.use('seaborn-whitegrid')
 
@@ -12,7 +11,14 @@ ours_csv = './ours.csv'
 x = []
 y_spos = []
 y_ours = []
-choice = 13
+# x_time_label = ['0', '5,000', '10,000', '15,000', '20,000', '25,000', '30,000',
+#                 '35,000', '40,000', '45,000', '50,000', '55,000']
+x_time_label = [
+    '0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'
+]
+
+from_idx = 1
+choice = 12
 max_second = 40000
 x_list = np.linspace(0, 40000, 21)
 
@@ -47,39 +53,42 @@ def smooth_list(value_list: list):
 # random, flops, params, snip, synflow, zennas, nwot
 zc_proxies = [-0.26, 0.5292, 0.5664, 0.5116, 0.5883, 0.4880, 0.5513, 0.3405]
 zc_label = [
-    'random', 'flops', 'params', 'snip', 'synflow', 'zennas', 'nwot', 'grasp'
+    'Random', 'FLOPs', 'Params', 'Snip', 'Synflow', 'ZenNAS', 'Nwot', 'Grasp'
 ]
-zc_marker = ['.', ',', 'o', 'v', '*', '<', '>', '^']
-zc_color = ['g', 'b', 'k', 'y', 'r', 'orange', 'g', 'grey']
+zc_marker = ['X', ',', 'o', 'v', 'D', 'p', '>', '^']
+zc_color = ['mediumblue', 'b', 'k', 'y', 'r', 'orange', 'g', 'grey']
 
 y_spos = smooth_list(y_spos)
 y_ours = smooth_list(y_ours)
 
 if choice is not None:
-    x = x[:choice]
-    y_spos = y_spos[:choice]
-    y_ours = y_ours[:choice]
+    x = x[from_idx:choice]
+    y_spos = y_spos[from_idx:choice]
+    y_ours = y_ours[from_idx:choice]
+    x_time_label = x_time_label[from_idx:]
 
 plt.figure(figsize=(12, 9), dpi=100)
-plt.grid(True, linestyle='--', alpha=0.5)
+plt.grid(True, linestyle='-', alpha=0.9)
 
 plt.plot(
     x,
     y_spos,
     color='g',
-    mfc='white',
-    linewidth=2,
+    # mfc='white',
+    linewidth=4,
     marker='o',
-    linestyle='dashed',
-    label='spos')
+    linestyle=':',
+    label='SPOS',
+    markersize=12)
 plt.plot(
     x,
     y_ours,
     color='r',
-    mfc='white',
-    linewidth=2,
-    marker='o',
-    label='rd-nas')
+    # mfc='white',
+    linewidth=4,
+    marker='v',
+    markersize=12,
+    label='RD-NAS')
 
 # zc
 for i, zc in enumerate(zc_proxies):
@@ -87,17 +96,35 @@ for i, zc in enumerate(zc_proxies):
         0,
         zc,
         c=zc_color[i],
-        s=50,
+        s=80,
         cmap='viridis',
         marker=zc_marker[i],
         label=zc_label[i])
 
-plt.yticks(fontproperties='Times New Roman', size=20)
-plt.xticks(fontproperties='Times New Roman', size=20)
-plt.xlim(-1000, 25000)
-plt.ylim(-0.3, 0.8)
+plt.yticks(fontproperties='Times New Roman', size=27)
+plt.xticks(x, x_time_label, fontproperties='Times New Roman', size=27)
+plt.title(
+    'Rank Consistency of NAS-Bench-201 CIFAR10',
+    fontproperties='Times New Roman',
+    size=33)
+plt.xlim(-1000, 22500)
+# plt.ylim(-0.35, 0.8)
+plt.ylim(0, 0.8)
 
-plt.legend(fontsize='large')
-plt.xlabel('Time (sec)', fontdict={'family': 'Times New Roman', 'size': 18})
-plt.ylabel('Kendall tau', fontdict={'family': 'Times New Roman', 'size': 18})
+plt.annotate(
+    'Ours',
+    xy=(5000, 0.63),
+    fontsize=28,
+    c='r',
+    fontproperties='Times New Roman')
+# plt.annotate('100% faster', xy=(6000, 0.6), xytext=(12000, 0.6),
+#              arrowprops=dict(facecolor='r', shrink=0.01, ec='r'), fontsize=28, c='r')
+
+plt.legend(fontsize=22, loc=4)
+plt.xlabel(
+    '$10^3$Time (seconds)', fontdict={
+        'family': 'Times New Roman',
+        'size': 33
+    })
+plt.ylabel("Kendall's Tau", fontdict={'family': 'Times New Roman', 'size': 33})
 plt.show()
