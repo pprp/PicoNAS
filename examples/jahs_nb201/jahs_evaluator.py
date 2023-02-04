23fbb4920fa6c5b78a20e167005aa998f9333e63
PJDong <1115957667@qq.com>
2023-02-04 17:14:44 +0800

    [init] init the experiments of jahs

---
+++ examples/jahs_nb201/jahs_evaluator.py
@@ -0,0 +1,301 @@
+from typing import List, Union
+
+import numpy as np
+import torch.nn.functional as F
+from nas_201_api import NASBench201API as API
+from tqdm import tqdm
+
+from piconas.evaluator.base import Evaluator
+from piconas.nas.mutators import DiffMutator, OneShotMutator
+from piconas.predictor.pruners.predictive import find_measures
+from piconas.utils.rank_consistency import (concordant_pair_ratio, kendalltau,
+                                            minmax_n_at_k, p_at_tb_k, pearson,
+                                            rank_difference, spearman)
+
+
+class JAHSEvaluator(Evaluator):
+    """Evaluate the NB201 Benchmark
+
+    Args:
+        trainer (NB201Trainer): _description_
+        num_sample (int, optional): _description_. Defaults to None.
+        search_space (str, optional): _description_. Defaults to 'nasbench201'.
+        dataset (str, optional): _description_. Defaults to 'cifar10'.
+        type (str, optional): _description_. Defaults to 'eval_acc1es'.
+    """
+
+    def __init__(self,
+                 trainer,
+                 num_sample: int = None,
+                 dataset: str = 'cifar10',
+                 type: str = 'eval_acc1es'):
+        super().__init__(trainer=trainer, dataset=dataset)
+        self.trainer = trainer
+        self.num_sample = num_sample
+        self.type = type
+        self.search_space = 'nasbench201'
+        self.dataset = dataset
+
+        if dataset == 'cifar10':
+            self.num_classes = 10
+        elif dataset == 'cifar100':
+            self.num_classes = 100
+        elif dataset == 'ImageNet16-120':
+            self.num_classes = 120
+            self.dataset = 'ImageNet16-120'
+
+        assert type in ['train_losses', 'eval_losses',
+                        'train_acc1es', 'eval_acc1es', 'cost_info'], \
+            f'Not support type {type}.'
+        """
+        The key of api is the genotype of nb201
+            such as '|avg_pool_3x3~0|+
+                     |nor_conv_1x1~0|skip_connect~1|+
+                     |nor_conv_1x1~0|skip_connect~1|skip_connect~2|'
+        The value of api is also a dict:
+                  key: cifar10-valid
+                  value: train_losses, eval_losses, train_acc1es
+                         eval_acc1es, cost_info
+        """
+        self.api = API(
+            './data/benchmark/NAS-Bench-201-v1_0-e61699.pth', verbose=False)
+
+    def generate_genotype(self, subnet_dict: dict,
+                          mutator: Union[OneShotMutator, DiffMutator]) -> str:
+        """subnet_dict represent the subnet dict of mutator."""
+        # Please make sure that the mutator have been called the
+        # `prepare_from_supernet` function.
+        alias2group_id = mutator.alias2group_id
+        genotype = ''
+        for i, (k, v) in enumerate(subnet_dict.items()):
+            # v = 'nor_conv_3x3'
+            alias_name = list(alias2group_id.keys())[k]
+            rank = alias_name.split('_')[1][-1]  # 0 or 1 or 2
+            genotype += '|'
+            genotype += f'{v}~{rank}'
+            genotype += '|'
+            if i in [0, 2]:
+                genotype += '+'
+        genotype = genotype.replace('||', '|')
+        return genotype
+
+    def generate_subnet(self, genotype: str):
+        """|avg_pool_3x3~0|+|nor_conv_1x1~0|skip_connect~1|+|nor_conv_1x1~0|skip_connect~1|skip_connect~2|"""
+        assert genotype is not None
+        genotype = genotype.replace('|+|', '|')
+        geno_list = genotype.split('|')[1:-1]
+        subnet_dict = dict()
+        for i, geno in enumerate(geno_list):
+            subnet_dict[i] = geno.split('~')[0]
+        return subnet_dict
+
+    def query_result(self, genotype: str, cost_key: str = 'flops'):
+        """query the indictor by genotype."""
+        dataset = self.trainer.dataset
+        index = self.api.query_index_by_arch(genotype)
+        # TODO
+        xinfo = self.api.get_more_info(index, 'cifar10-valid', hp='200')
+        # TODO
+        return xinfo['valid-accuracy']
+
+    def compute_rank_consistency(self, dataloader,
+                                 mutator: OneShotMutator) -> List:
+        """compute rank consistency of different types of indicators."""
+        true_indicator_list: List[float] = []
+        generated_indicator_list: List[float] = []
+        flops_indicator_list: List[float] = []
+        # for cpr
+        subtract_true_list: List[float] = []
+        subtract_indicator_list: List[float] = []
+
+        self.trainer.logger.info('Begin to compute rank consistency...')
+        num_sample = 50 if self.num_sample is None else self.num_sample
+
+        for _ in range(num_sample):
+            # sample random subnet by mutator
+            random_subnet_dict_ = mutator.random_subnet
+
+            # process for search space shrink and expand
+            random_subnet_dict = dict()
+            for k, v in random_subnet_dict_.items():
+                random_subnet_dict[k] = v.rstrip('_')
+
+            # get true indictor by query nb201 api
+            genotype = self.generate_genotype(random_subnet_dict, mutator)
+            results = self.query_result(genotype)  # type is eval_acc1es
+            true_indicator_list.append(results)
+
+            # get score based on supernet
+            score = self.trainer.metric_score(dataloader, random_subnet_dict)
+            generated_indicator_list.append(score)
+
+            # get flops
+            flops_result = self.query_result(genotype, cost_key='flops')
+            flops_indicator_list.append(flops_result)
+
+            # sample another subnet pair for cpr
+            random_subnet_dict2 = self.trainer.mutator.random_subnet
+            self.trainer.mutator.set_subnet(random_subnet_dict2)
+            genotype = self.generate_genotype(random_subnet_dict,
+                                              self.trainer.mutator)
+            results2 = self.query_result(genotype)  # type is eval_acc1es
+            subtract_true_list.append(results - results2)
+
+            score2 = self.trainer.metric_score(dataloader, random_subnet_dict)
+            subtract_indicator_list.append(score - score2)
+
+        return self.calc_results(true_indicator_list, generated_indicator_list,
+                                 flops_indicator_list, subtract_true_list,
+                                 subtract_indicator_list)
+
+    def compute_overall_rank_consistency(self, dataloader) -> List:
+        """compute rank consistency of all search space."""
+        true_indicator_list: List[float] = []
+        generated_indicator_list: List[float] = []
+        flops_indicator_list: List[float] = []
+
+        self.trainer.logger.info('Begin to compute rank consistency...')
+
+        for genotype in list(self.api.keys()):
+            if 'none' in genotype:
+                continue
+
+            subnet_dict = self.generate_subnet(genotype)
+
+            # get true indictor by query nb201 api
+            results = self.query_result(genotype)  # type is eval_acc1es
+            true_indicator_list.append(results)
+
+            # get score based on supernet
+            results = self.trainer.metric_score(dataloader, subnet_dict)
+            generated_indicator_list.append(results)
+
+            # get flops
+            flops_result = self.query_result(genotype, cost_key='flops')
+            flops_indicator_list.append(flops_result)
+
+        return self.calc_results(true_indicator_list, generated_indicator_list,
+                                 flops_indicator_list)
+
+    def compute_rank_by_predictive(self,
+                                   dataloader=None,
+                                   measure_name: List = ['flops']) -> List:
+        """compute rank consistency by zero cost metric."""
+        true_indicator_list: List[float] = []
+        generated_indicator_list: List[float] = []
+        flops_indicator_list: List[float] = []
+        # for cpr
+        subtract_true_list: List[float] = []
+        subtract_indicator_list: List[float] = []
+
+        if dataloader is None:
+            from piconas.datasets import build_dataloader
+            dataloader = build_dataloader('cifar10', 'train')
+
+        num_sample = 50 if self.num_sample is None else self.num_sample
+
+        for _ in tqdm(range(num_sample)):
+            # sample random subnet by mutator
+            random_subnet_dict = self.trainer.mutator.random_subnet
+            self.trainer.mutator.set_subnet(random_subnet_dict)
+
+            # get true indictor by query nb201 api
+            genotype = self.generate_genotype(random_subnet_dict,
+                                              self.trainer.mutator)
+            results = self.query_result(genotype)  # type is eval_acc1es
+            true_indicator_list.append(results)
+
+            assert isinstance(measure_name, list) and len(measure_name) == 1, \
+                f'The measure name should be a list but got {type(measure_name)}' \
+                f' and the lenght should be 1 but got {len(measure_name)}'
+
+            dataload_info = ['random', 3, self.num_classes]
+
+            # get predict indicator by predictive.
+            score = find_measures(
+                self.trainer.model,
+                dataloader,
+                dataload_info=dataload_info,
+                measure_names=measure_name,
+                loss_fn=F.cross_entropy,
+                device=self.trainer.device)
+            generated_indicator_list.append(score)
+
+            # get score based on flops
+            tmp_type = self.type
+            self.type = 'cost_info'
+            results = self.query_result(genotype, cost_key='flops')
+            flops_indicator_list.append(results)
+            self.type = tmp_type
+
+            # add another pair for cpr
+            random_subnet_dict2 = self.trainer.mutator.random_subnet
+            self.trainer.mutator.set_subnet(random_subnet_dict2)
+            genotype = self.generate_genotype(random_subnet_dict,
+                                              self.trainer.mutator)
+            results2 = self.query_result(genotype)  # type is eval_acc1es
+            subtract_true_list.append(results - results2)
+
+            score2 = find_measures(
+                self.trainer.model,
+                dataloader,
+                dataload_info=dataload_info,
+                measure_names=measure_name,
+                loss_fn=F.cross_entropy,
+                device=self.trainer.device)
+            subtract_indicator_list.append(score - score2)
+
+        return self.calc_results(true_indicator_list, generated_indicator_list,
+                                 flops_indicator_list, subtract_true_list,
+                                 subtract_indicator_list)
+
+    def calc_results(self,
+                     true_indicator_list,
+                     generated_indicator_list,
+                     flops_indicator_list=None,
+                     subtract_true_list=None,
+                     subtract_indicator_list=None):
+
+        kt = kendalltau(true_indicator_list, generated_indicator_list)
+        ps = pearson(true_indicator_list, generated_indicator_list)
+        sp = spearman(true_indicator_list, generated_indicator_list)
+        minn_at_ks = minmax_n_at_k(true_indicator_list,
+                                   generated_indicator_list)
+        patks = p_at_tb_k(true_indicator_list, generated_indicator_list)
+
+        cpr = -1
+        if subtract_indicator_list is not None:
+            cpr = concordant_pair_ratio(true_indicator_list,
+                                        generated_indicator_list)
+
+        if flops_indicator_list is not None:
+            # calculate rank difference by range.
+
+            # compute rank diff in 5 section with different flops range.
+            sorted_idx_by_flops = np.array(flops_indicator_list).argsort()
+
+            # compute splited index by flops
+            range_length = len(sorted_idx_by_flops) // 5
+            splited_idx_by_flops = [
+                sorted_idx_by_flops[i * range_length:(i + 1) * range_length]
+                for i in range(
+                    int(len(sorted_idx_by_flops) / range_length) + 1)
+                if (sorted_idx_by_flops[i * range_length:(i + 1) *
+                                        range_length]).any()
+            ]
+
+            true_indicator_list = np.array(true_indicator_list)
+            generated_indicator_list = np.array(generated_indicator_list)
+
+            rd_list = []
+            for splited_idx_by_flop in splited_idx_by_flops:
+                current_idx = np.array(splited_idx_by_flop)
+                tmp_rd = rank_difference(true_indicator_list[current_idx],
+                                         generated_indicator_list[current_idx])
+                rd_list.append(tmp_rd)
+
+        print(
+            f"Kendall's tau: {kt}, pearson coeff: {ps}, spearman coeff: {sp}, rank diff: {rd_list}, cpr: {cpr}."
+        )
+
+        return kt, ps, sp, rd_list, minn_at_ks, patks, cpr
---
+++ examples/jahs_nb201/jahs_ranker.py
@@ -0,0 +1,211 @@
+import argparse
+import os
+import time
+
+import torch
+
+import piconas.utils.utils as utils
+from piconas.core import build_criterion, build_optimizer, build_scheduler
+from piconas.datasets.build import build_dataloader
+from piconas.evaluator import NB201Evaluator
+from piconas.models import build_model
+from piconas.trainer import build_trainer
+from piconas.utils.config import Config
+
+
+def get_args():
+    parser = argparse.ArgumentParser('train macro benchmark')
+    parser.add_argument(
+        '--config',
+        type=str,
+        default='configs/spos/spos_cifar10.py',
+        required=False,
+        help='user settings config',
+    )
+    parser.add_argument(
+        '--work_dir', type=str, default='./work_dir', help='experiment name')
+    parser.add_argument(
+        '--data_dir',
+        type=str,
+        default='./data/cifar',
+        help='path to the dataset')
+
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        default='OneShotNASBench201Network',
+        help='name of model')
+    parser.add_argument(
+        '--trainer_name',
+        type=str,
+        default='NB201Trainer',
+        help='name of trainer')
+    parser.add_argument(
+        '--log_name',
+        type=str,
+        default='eval_nb201_rank',
+        help='name of this experiments',
+    )
+
+    parser.add_argument(
+        '--crit', type=str, default='mse', help='decide the criterion')
+    parser.add_argument(
+        '--optims', type=str, default='sgd', help='decide the optimizer')
+    parser.add_argument(
+        '--sched', type=str, default='cosine', help='decide the scheduler')
+
+    parser.add_argument(
+        '--classes', type=int, default=10, help='dataset classes')
+    parser.add_argument('--layers', type=int, default=20, help='batch size')
+    parser.add_argument(
+        '--num_choices', type=int, default=4, help='number choices per layer')
+    parser.add_argument(
+        '--batch_size', type=int, default=256, help='batch size')
+    parser.add_argument('--epochs', type=int, default=200, help='batch size')
+    parser.add_argument(
+        '--lr', type=float, default=0.025, help='initial learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+    parser.add_argument(
+        '--weight-decay', type=float, default=5e-4, help='weight decay')
+    parser.add_argument(
+        '--val_interval',
+        type=int,
+        default=5,
+        help='validate and save frequency')
+    parser.add_argument(
+        '--random_search',
+        type=int,
+        default=1000,
+        help='validate and save frequency')
+    # ******************************* dataset *******************************#
+    parser.add_argument(
+        '--dataset', type=str, default='cifar10', help='path to the dataset')
+    parser.add_argument('--cutout', action='store_true', help='use cutout')
+    parser.add_argument(
+        '--cutout_length', type=int, default=16, help='cutout length')
+    parser.add_argument(
+        '--auto_aug',
+        action='store_true',
+        default=False,
+        help='use auto augmentation')
+    parser.add_argument(
+        '--resize', action='store_true', default=False, help='use resize')
+    parser.add_argument(
+        '--ckpt_path', type=str, default='', help='checkpoint path')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    # merge argparse and config file
+    if args.config is not None:
+        cfg = Config.fromfile(args.config)
+        cfg.merge_from_dict(vars(args))
+    else:
+        cfg = Config(args)
+
+    print(cfg)
+
+    # dump config files
+    cfg.work_dir = os.path.join(cfg.work_dir, cfg.trainer_name)
+    if not os.path.exists(cfg.work_dir):
+        os.makedirs(cfg.work_dir)
+    current_exp_name = f'{cfg.model_name}-{cfg.trainer_name}-{cfg.log_name}.yaml'
+    cfg.dump(os.path.join(cfg.work_dir, current_exp_name))
+
+    if torch.cuda.is_available():
+        print('Train on GPU!')
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    # train_dataloader = build_dataloader(
+    #     type='train', dataset=cfg.dataset, config=cfg)
+
+    val_dataloader = build_dataloader(
+        type='val', dataset=cfg.dataset, config=cfg)
+
+    if cfg.dataset == 'cifar10':
+        num_classes = 10
+    elif cfg.dataset == 'cifar100':
+        num_classes = 100
+    elif cfg.dataset == 'ImageNet16-120':
+        num_classes = 120
+    else:
+        raise NotImplementedError(
+            f'Not Support Type of datasets: {cfg.dataset}.')
+
+    model = build_model(cfg.model_name, num_classes=num_classes)
+
+    criterion = build_criterion(cfg.crit).to(device)
+    optimizer = build_optimizer(model, cfg)
+    scheduler = build_scheduler(cfg, optimizer)
+
+    # load checkpoint
+    assert cfg.ckpt_path is not None
+
+    for k, v in model.state_dict().items():
+        print(k)
+
+    print('===' * 30)
+
+    state_dict = torch.load(cfg.ckpt_path)['state_dict']
+    treated_sd = dict()
+    for k, v in state_dict.items():
+        if 'conv_3x3' in k:
+            new_k = k.replace('conv_3x3', 'nor_conv_3x3')
+        elif 'conv_1x1' in k:
+            new_k = k.replace('conv_1x1', 'nor_conv_1x1')
+        else:
+            new_k = k
+        print(new_k)
+        treated_sd[new_k] = v
+
+    model.load_state_dict(treated_sd)
+
+    model = model.to(device)
+
+    trainer = build_trainer(
+        cfg.trainer_name,
+        model=model,
+        mutator=None,
+        optimizer=optimizer,
+        criterion=criterion,
+        scheduler=scheduler,
+        searching=True,
+        device=device,
+        log_name=cfg.log_name,
+    )
+
+    start = time.time()
+
+    evaluator = NB201Evaluator(trainer, num_sample=1000)
+
+    kt, pt, sp, rd_list, minn_at_ks, patks = \
+        evaluator.compute_rank_consistency(dataloader=val_dataloader,
+                                           mutator=trainer.mutator)
+
+    print('==' * 20)
+    print('=>>> overall rank consistency')
+    print(f'Kendall tau: {kt} Pearson coeff: {pt} Spearman: {sp}')
+    print('=>>> overall rank difference')
+    for i, rd in enumerate(rd_list):
+        print(f'RD_{20*i}%: {rd}')
+    print('=>>> BR@K and WR@K')
+    for k, minn, brk, maxn, wrk in minn_at_ks:
+        print(f'{k}_BR@K: {brk} {k}_WR@K: {wrk}')
+    print('=>>> KD@topK and KD@bottomK')
+    for ratio, k, p_at_topk, p_at_bk, kd_at_topk, kd_at_bk in patks:
+        print(f'{ratio}_KD@topk: {kd_at_topk} {ratio}_KD@bottomK: {kd_at_bk}')
+
+    print('=>>> P@topK and P@bottomK')
+    for ratio, k, p_at_topk, p_at_bk, kd_at_topk, kd_at_bk in patks:
+        print(f'{ratio}_P@topk: {p_at_topk} {ratio}_P@bottomK: {p_at_bk}')
+    print('==' * 20)
+
+    utils.time_record(start)
+
+
+if __name__ == '__main__':
+    main()
